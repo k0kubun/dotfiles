6a0572a3002772a9b827806dc0140aa1e5532240
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-11-01 18:18:27 +0900

    Switch to MItamae v0.5.0

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+set -e
 
 if ! [ -d config/.vim/bundle/neobundle.vim/.git ]; then
   git submodule init && git submodule update --depth 1
 fi
 
-sudo bin/itamae local lib/recipe.rb
+bin/setup-mitamae
+
+# Homebrew does not allow sudo.
+case "$(uname)" in
+  "Darwin") bin/mitamae local lib/recipe.rb ;;
+  *)   sudo bin/mitamae local lib/recipe.rb ;;
+esac
--- lib/recipe_helper.rb
+++ lib/recipe_helper.rb
@@ -1,4 +1,4 @@
-Itamae::RecipeContext.class_eval do
+MItamae::RecipeContext.class_eval do
   def include_cookbook(name)
     root_dir = File.expand_path('../..', __FILE__)
     include_recipe File.join(root_dir, 'cookbooks', name, 'default')
