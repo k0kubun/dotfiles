a185cc8e3c108eb3761e1127ae77f789f9121a38
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-12-09 19:25:28 +0900

    Add Slack bindings

--- config/config.py
+++ config/config.py
@@ -27,7 +27,15 @@ def configure(keymap):
     keymap_iterm2["Cmd-P"] = "Alt-P"
     keymap_iterm2["Cmd-W"] = "Alt-W"
 
-    # Activation of specific window
+    # Slack
+    keymap_slack = keymap.defineWindowKeymap(app_name="com.tinyspeck.slackmacgap")
+    keymap_slack["Cmd-N"] = "Cmd-K"
+    keymap_slack["Cmd-J"] = "Alt-Down"
+    keymap_slack["Cmd-K"] = "Alt-Up"
+    keymap_slack["Cmd-Ctrl-J"] = "Alt-Shift-Down"
+    keymap_slack["Cmd-Ctrl-K"] = "Alt-Shift-Up"
+
+    # Application Launcher
     keymap_global["Ctrl-H"] = keymap.ActivateApplicationCommand("com.googlecode.iterm2" )
     keymap_global["Ctrl-O"] = keymap.ActivateApplicationCommand("com.tinyspeck.slackmacgap" )
     keymap_global["Ctrl-U"] = keymap.ActivateApplicationCommand("com.google.Chrome" )
