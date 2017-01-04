d9d3b25f3b8fbc761f54c381739f5f726241b631
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-04 22:35:59 +0900

    Split command

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -20,9 +20,14 @@ define :github_binary, version: nil, repository: nil, archive: nil, binary_path:
     raise "unexpected ext archive: #{archive}"
   end
 
-  binary_path = params[:binary_path] || cmd
-  execute "curl -fsSL -o /tmp/#{archive} #{url} && #{extract} /tmp/#{archive} && mv /tmp/#{binary_path} #{path} && chmod +x #{path}" do
+  execute "curl -fSL -o /tmp/#{archive} #{url}" do
+    not_if "which #{cmd}"
+  end
+  execute "#{extract} /tmp/#{archive}" do
     not_if "which #{cmd}"
     cwd "/tmp"
+  end
+  execute " mv /tmp/#{params[:binary_path] || cmd} #{path} && chmod +x #{path}" do
+    not_if "which #{cmd}"
   end
 end
