4badf0318a306616e3efb8800ef4133a315f3c83
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-05 22:23:15 +0900

    Update 1pass comment

--- cookbooks/1password/default.rb
+++ cookbooks/1password/default.rb
@@ -2,9 +2,11 @@
 if run_command('which wine', error: false).exit_status == 0
   package 'wine'
 
-  # 1. Download 1Password.exe from https://agilebits.com/downloads
-  # 2. Execute it
-  # 3. Open `regedit` and create REG_DWORD "HKEY_CURRENT_USER\Software\AgileBits\1Password 4" with 0.
+  # 1. Do `winecfg` to update dpy
+  # 2. Download 1Password.exe from https://agilebits.com/downloads
+  # 3. Execute it
+  # 4. Open `regedit` and create VerifyCodeSignature,
+  #    REG_DWORD "HKEY_CURRENT_USER\Software\AgileBits\1Password 4" with 0.
 
   remote_file "#{ENV['HOME']}/.config/systemd/user/agile1pagent.service" do
     source 'files/agile1pagent.service'
