1e8778370d6227a26a20a84551f6262bb638d379
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-05 23:37:30 +0900

    Automate 1password installation

--- cookbooks/1password/default.rb
+++ cookbooks/1password/default.rb
@@ -1,8 +1,26 @@
 package 'wine'
 
-# 1. Do `winecfg` to update dpy
-# 2. Download 1Password.exe from https://agilebits.com/downloads
-# 3. Execute it
+exe_url   = "https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.1.617.exe"
+exe_sha1  = "1f7cd4115b1d0908765d97289e0d31a77dbe8948"
+dest_path = "#{ENV['HOME']}/.wine/drive_c/Program Files (x86)/1Password 4/1Password.exe"
+
+execute "curl -fSL -o /tmp/1Password.exe #{exe_url}" do
+  user node[:user]
+  not_if "test -f #{dest_path.shellescape}"
+  verify "echo #{exe_sha1}  /tmp/1Password.exe | sha1sum --status --check -"
+end
+
+execute "wine /tmp/1Password.exe" do
+  user node[:user]
+  not_if "test -f #{dest_path.shellescape}"
+end
+
+local_ruby_block 'wait wine installation' do
+  block do
+    sleep 1 until File.exist?(dest_path)
+  end
+  not_if "test -f #{dest_path.shellescape}"
+end
 
 file "#{ENV['HOME']}/.wine/user.reg" do
   action :edit
