73509cee2479bf763c4d1ced3e8eb1d34d12802e
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-05 22:46:18 +0900

    Automate regedit

--- cookbooks/1password/default.rb
+++ cookbooks/1password/default.rb
@@ -5,8 +5,19 @@ if run_command('which wine', error: false).exit_status == 0
   # 1. Do `winecfg` to update dpy
   # 2. Download 1Password.exe from https://agilebits.com/downloads
   # 3. Execute it
-  # 4. Open `regedit` and create VerifyCodeSignature,
-  #    REG_DWORD "HKEY_CURRENT_USER\Software\AgileBits\1Password 4" with 0.
+
+  file "#{ENV['HOME']}/.wine/user.reg" do
+    action :edit
+    block do |content|
+      config = '"VerifyCodeSignature"=dword:00000000'
+      unless content.include?(config)
+        content.gsub!(
+          /^\[Software\\\\AgileBits\\\\1Password 4\].*\n("[A-U].*\n)*/,
+          "\\0#{config}\n",
+        )
+      end
+    end
+  end
 
   remote_file "#{ENV['HOME']}/.config/systemd/user/agile1pagent.service" do
     source 'files/agile1pagent.service'
