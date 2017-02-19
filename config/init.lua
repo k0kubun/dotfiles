3a4a983aa3f79fc040b15c4358b8692e2237acc8
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-19 12:54:44 +0900

    Add keyremapper for tmux window switcher

--- config/init.lua
+++ config/init.lua
@@ -1,9 +1,3 @@
-local function invoke(app)
-  return function()
-    hs.execute("open '/Applications/"..app..".app'")
-  end
-end
-
 -- hs.hotkey.bind(mods, key, message, pressedfn, releasedfn, repeatfn) -> hs.hotkey object
 -- Parameters:
 --   mods - A table or a string containing (as elements, or as substrings with any separator) the keyboard modifiers required, which should be zero or more of the following:
@@ -16,6 +10,25 @@ end
 --   pressedfn - A function that will be called when the hotkey has been pressed, or nil
 --   releasedfn - A function that will be called when the hotkey has been released, or nil
 --   repeatfn - A function that will be called when a pressed hotkey is repeating, or nil
-hs.hotkey.bind({'ctrl'}, 'h', nil, invoke('Utilities/Terminal'), nil, nil)
-hs.hotkey.bind({'ctrl'}, 'o', nil, invoke('Nocturn'), nil, nil)
-hs.hotkey.bind({'ctrl'}, 'u', nil, invoke('Google Chrome'), nil, nil)
+
+local function openApp(app)
+  local command = "open '/Applications/"..app..".app'"
+  return function()
+    hs.execute(command)
+  end
+end
+
+local function inputKey(modifiers, key)
+  return function()
+    hs.eventtap.event.newKeyEvent(modifiers, key, true):post()
+    hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent(modifiers, key, false):post()
+  end
+end
+
+hs.hotkey.bind({'ctrl'}, 'h', nil, openApp('Utilities/Terminal'), nil, nil)
+hs.hotkey.bind({'ctrl'}, 'o', nil, openApp('Nocturn'), nil, nil)
+hs.hotkey.bind({'ctrl'}, 'u', nil, openApp('Google Chrome'), nil, nil)
+
+hs.hotkey.bind({'cmd'}, 'o', nil, inputKey({'alt'}, 'o'), nil, nil)
+hs.hotkey.bind({'cmd'}, 'p', nil, inputKey({'alt'}, 'p'), nil, nil)
