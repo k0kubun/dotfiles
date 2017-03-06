ca0d10b7e933ee0c6a7da412d33a7ab0d5760d0e
Takashi Kokubun <takashikkbn@gmail.com>
2017-03-06 20:32:07 +0900

    Refactor ghq cookbook

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -1,3 +1,7 @@
+node.reverse_merge!(
+  os: run_command('uname').stdout.strip.downcase,
+)
+
 define :dotfile, source: nil do
   source = params[:source] || params[:name]
   link File.join(ENV['HOME'], params[:name]) do
--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,11 +1,7 @@
 github_binary 'ghq' do
   repository 'motemen/ghq'
   version 'v0.7.4'
-  if node[:platform] == 'darwin'
-    archive 'ghq_darwin_amd64.zip'
-  else
-    archive 'ghq_linux_amd64.zip'
-  end
+  archive "ghq_#{node[:os]}_amd64.zip"
 end
 
 include_cookbook 'git'
