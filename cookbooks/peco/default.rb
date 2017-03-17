198bbf4a6f3cf7ff3d809fd4f2c38dbb175d5805
Takashi Kokubun <takashikkbn@gmail.com>
2017-03-17 13:10:39 +0900

    Refactor peco cookbook

--- cookbooks/peco/default.rb
+++ cookbooks/peco/default.rb
@@ -1,13 +1,8 @@
 github_binary 'peco' do
   repository 'peco/peco'
   version 'v0.4.7'
-  if node[:platform] == 'darwin'
-    archive 'peco_darwin_amd64.zip'
-    binary_path 'peco_darwin_amd64/peco'
-  else
-    archive 'peco_linux_amd64.tar.gz'
-    binary_path 'peco_linux_amd64/peco'
-  end
+  archive "peco_#{node[:os]}_amd64.zip"
+  binary_path "peco_#{node[:os]}_amd64/peco"
 end
 
 dotfile '.peco'
