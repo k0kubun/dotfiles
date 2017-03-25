3c3c88cc7b4286890dc63d91b03248bd1d0f776f
Takashi Kokubun <takashikkbn@gmail.com>
2017-03-25 22:03:52 +0900

    Show prompt in proper prompt

--- config/.zsh/lib/theme.zsh
+++ config/.zsh/lib/theme.zsh
@@ -1,21 +1,24 @@
-# zsh prompt
-PROMPT='$ '
 autoload -Uz vcs_info
+LANG=en_US.UTF-8 vcs_info
+
 precmd() {
   local last_status="$?"
-  LANG=en_US.UTF-8 vcs_info
 
   local left1="$(pwd) "
   local left2="status: $last_status"
   local right1="$vcs_info_msg_0_"
 
-  local left1c="\e[36m${left1}\e[m"
-  if [[ "x$last_status" == "x0" ]]; then
-    local left2c="${left2}"
-  else
-    local left2c="\e[31m${left2}\e[m"
-  fi
-  local right1c="\e[32m${right1}\e[m"
+  psvar=()
+  psvar[1]="${left1}"
+  psvar[2]="${left2}"
+  psvar[3]="${(r:($COLUMNS-${#left1}-${#left2}-${#right1}):: :)}"
+  psvar[4]="${right1}"
 
-  echo -e "${left1c}${left2c}${(r:($COLUMNS-${#left1}-${#left2}-${#right1}):: :)}${right1c}"
+  if [[ "x$last_status" == "x0" ]]; then
+    PROMPT="%F{cyan}%1v%f%2v%3v%F{green}%4v%f
+$ "
+  else
+    PROMPT="%F{cyan}%1v%f%F{red}%2v%f%3v%F{green}%4v%f
+$ "
+  fi
 }
