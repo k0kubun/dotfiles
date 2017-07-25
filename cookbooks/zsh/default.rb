851a2bde33f9b523703791a5787af9384a04ae2a
Takashi Kokubun <takashikkbn@gmail.com>
2017-07-25 12:17:58 +0900

    Drop sudo requirement from macOS

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,12 +1,12 @@
 if node[:platform] != 'darwin'
   package 'zsh'
   dotfile '.zshrc.Linux'
+
+  execute "chsh -s /bin/zsh #{node[:user]}" do
+    only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
+    user 'root'
+  end
 end
 
 dotfile '.zsh'
 dotfile '.zshrc'
-
-execute "chsh -s /bin/zsh #{node[:user]}" do
-  only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
-  user 'root'
-end
