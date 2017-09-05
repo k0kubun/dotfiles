fb41a272d55b6d6e01f01f9d57191f3f11797aef
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-05 19:31:41 +0900

    Use .tar.gz for linux

--- cookbooks/peco/default.rb
+++ cookbooks/peco/default.rb
@@ -1,7 +1,8 @@
 github_binary 'peco' do
   repository 'peco/peco'
   version 'v0.5.1'
-  archive "peco_#{node[:os]}_amd64.zip"
+  ext = (node[:platform] == 'darwin' ? 'zip' : 'tar.gz')
+  archive "peco_#{node[:os]}_amd64.#{ext}"
   binary_path "peco_#{node[:os]}_amd64/peco"
 end
 
