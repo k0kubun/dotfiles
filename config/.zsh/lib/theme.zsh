3f8ef168a6d7d0d33c8324eb3c52f51ff1ac5204
Takashi Kokubun <takashikkbn@gmail.com>
2017-11-11 13:32:12 +0900

    Show hostname on zsh prompt

--- config/.zsh/lib/theme.zsh
+++ config/.zsh/lib/theme.zsh
@@ -5,20 +5,22 @@ precmd() {
   LANG=en_US.UTF-8 vcs_info
 
   local left1="$(pwd) "
-  local left2="status: $last_status"
-  local right1="$vcs_info_msg_0_"
+	local left2="† $(hostname | sed -e 's/\.local$//') "
+  local left3="status: $last_status"
+	local right1="$vcs_info_msg_0_"
 
   psvar=()
   psvar[1]="${left1}"
   psvar[2]="${left2}"
-  psvar[3]="${(r:($COLUMNS-${#left1}-${#left2}-${#right1}):: :)}"
-  psvar[4]="${right1}"
+  psvar[3]="${left3}"
+  psvar[4]="${(r:($COLUMNS-${#left1}-${#left2}-${#left3}-${#right1}):: :)}"
+  psvar[5]="${right1}"
 
   if [[ "x$last_status" == "x0" ]]; then
-    PROMPT="%F{cyan}%1v%f%2v%3v%F{green}%4v%f
+    PROMPT="%F{cyan}%1v%f%F{blue}%2v%f%3v%4v%F{green}%5v%f
 $ "
   else
-    PROMPT="%F{cyan}%1v%f%F{red}%2v%f%3v%F{green}%4v%f
+    PROMPT="%F{cyan}%1v%f%F{blue}%2v%f%F{red}%3v%f%4v%F{green}%5v%f
 $ "
-  fi
+	fi
 }
