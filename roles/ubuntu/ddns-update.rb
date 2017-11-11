db9399ab584cb8becb11cd412fc59b14f4797f1c
Takashi Kokubun <takashikkbn@gmail.com>
2017-11-11 19:03:32 +0900

    Implement ddns-update with Route53

--- roles/ubuntu/ddns-update.rb
+++ roles/ubuntu/ddns-update.rb
@@ -4,7 +4,7 @@ directory '/opt/local/ddns-update' do
   mode '755'
 end
 
-remote_file '/opt/local/ddns-update/update.sh' do
+remote_file '/opt/local/ddns-update/update' do
   owner node[:user]
   group node[:user]
   mode '775'
@@ -15,18 +15,6 @@ remote_file '/opt/local/ddns-update/current' do
   group node[:user]
   mode '644'
   not_if 'test -f /opt/local/ddns-update/current'
-end
-
-%w[
-  /opt/local/ddns-update/username
-  /opt/local/ddns-update/password
-].each do |f|
-  remote_file f do
-    owner node[:user]
-    group node[:user]
-    mode '600'
-    not_if "test -f #{f}"
-  end
 end
 
 remote_file "#{ENV['HOME']}/.config/systemd/user/ddns-update.service" do
@@ -43,4 +31,8 @@ end
 
 link "#{ENV['HOME']}/.config/systemd/user/timers.target.wants/ddns-update.timer" do
   to "#{ENV['HOME']}/.config/systemd/user/ddns-update.timer"
+end
+
+gem_package 'aws-sdk-route53' do
+  user 'root'
 end
