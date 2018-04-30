98e7d058d491a1665fec4e43494511cc31412f56
Takashi Kokubun <takashikkbn@gmail.com>
2018-05-01 00:29:27 +0900

    Simplify the ubuntu role

--- roles/ubuntu/default.rb
+++ roles/ubuntu/default.rb
@@ -1,22 +1,22 @@
 include_role 'base'
 
-include_cookbook 'urxvt'
-include_cookbook 'xremap'
+# include_cookbook 'urxvt'
+# include_cookbook 'xremap'
 include_cookbook 'skk'
 include_cookbook 'git'
 include_cookbook 'ghq'
 include_cookbook 'peco'
 include_cookbook 'zsh'
 include_cookbook 'tmux'
-include_cookbook 'vim'
-include_cookbook 'thunderbolt'
+# include_cookbook 'vim'
+# include_cookbook 'thunderbolt'
 
 if has_package?('wine')
   include_cookbook '1password'
 end
 
 dotfile '.Xmodmap'
-dotfile '.rake'
+# dotfile '.rake'
 dotfile '.gemrc'
 dotfile '.pryrc'
 dotfile '.railsrc'
@@ -26,12 +26,12 @@ include_recipe 'ssh-agent'
 # include_recipe 'ddns-update'
 
 # For TZ=UTC
-remote_file '/lib/systemd/system/mysql.service' do
-  owner 'root'
-  group 'root'
-  mode '644'
-  only_if 'which mysql'
-end
+# remote_file '/lib/systemd/system/mysql.service' do
+#   owner 'root'
+#   group 'root'
+#   mode '644'
+#   only_if 'which mysql'
+# end
 
 link "#{ENV['HOME']}/.git-template/hooks" do
   to "#{ENV['HOME']}/.githooks"
