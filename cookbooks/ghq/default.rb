b4d12bb8c2053dd21e7092e92d212e9a1615260b
Takashi Kokubun <takashikkbn@gmail.com>
2019-04-27 21:40:12 +0900

    Upgrade ghq to v0.10.0

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,7 +1,8 @@
 github_binary 'ghq' do
   repository 'motemen/ghq'
-  version 'v0.7.4'
+  version 'v0.10.0'
   archive "ghq_#{node[:os]}_amd64.zip"
+  binary_path "ghq_#{node[:os]}_amd64/ghq"
 end
 
 include_cookbook 'git'
