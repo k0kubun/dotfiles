d1b3376e276efe06d1042b6c634f2843ae31e372
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-08 14:25:18 +0900

    Drop wrong gpg-agent config

--- roles/ubuntu/gpg-agent.rb
+++ roles/ubuntu/gpg-agent.rb
@@ -9,16 +9,6 @@ remote_file "#{ENV['HOME']}/.gnupg/gpg-agent.conf" do
   group node[:user]
 end
 
-remote_file "#{ENV['HOME']}/.config/systemd/user/gpg-agent.service" do
-  source 'files/gpg-agent.service'
-  owner node[:user]
-  group node[:user]
-end
-
-link "#{ENV['HOME']}/.config/systemd/user/default.target.wants/gpg-agent.service" do
-  to "#{ENV['HOME']}/.config/systemd/user/gpg-agent.service"
-end
-
 user_service 'gpg-agent' do
   action :start
 end
