a3c2f14f7f1639030b297f787d27f70cd985a408
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-08 14:36:28 +0900

    Reuse default gpg-agent service

--- roles/ubuntu/gpg-agent.rb
+++ roles/ubuntu/gpg-agent.rb
@@ -1,3 +1,5 @@
+package 'gnupg-agent'
+
 directory "#{ENV['HOME']}/.gnupg" do
   owner node[:user]
   group node[:user]
@@ -9,6 +11,19 @@ remote_file "#{ENV['HOME']}/.gnupg/gpg-agent.conf" do
   group node[:user]
 end
 
+file '/usr/lib/systemd/user/gpg-agent.service' do
+  action :edit
+  block do |content|
+    unless content.include?('[Install]')
+      content.gsub!(/\z/, "\n[Install]\nWantedBy=default.target\n")
+    end
+  end
+end
+
 user_service 'gpg-agent' do
-  action :start
+  action [:start, :enable]
+end
+
+user_service 'gpg-agent.socket' do
+  action [:start, :enable]
 end
