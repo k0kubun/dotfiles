3e87d099cbbb1fb4bc485a5d6601b3338b2c3ed7
Takashi Kokubun <takashikkbn@gmail.com>
2020-06-16 21:46:03 -0700

    Force overwrite .gitconfig

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,4 +1,14 @@
 package 'git'
 
+if node[:platform] == 'darwin'
+  # Remove:
+  # [filesystem "Azul Systems, Inc.|1.8.0_222|/dev/disk1s5"]
+  # [filesystem "Oracle Corporation|1.8.0_221|/dev/disk1s5"]
+  file(gitconfig = File.join(ENV['HOME'], '.gitconfig')) do
+    action :delete
+    only_if { File.file?(gitconfig) && !File.symlink?(gitconfig) }
+  end
+end
+
 dotfile '.gitconfig'
 dotfile '.gitignore'
