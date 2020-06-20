8b48f72ac5eff1b1f2a87ee643a5c1309655cc58
Takashi Kokubun <takashikkbn@gmail.com>
2020-06-20 16:39:02 -0700

    Remove unused aliases

--- config/.zsh/lib/git.zsh
+++ config/.zsh/lib/git.zsh
@@ -4,9 +4,7 @@ alias gd="git --no-pager diff"
 alias ga="git commit -am"
 alias gh="git branch"
 alias co="git checkout"
-alias origin="git pull origin master"
 alias amend="git commit --amend"
-alias ch='git rev-parse HEAD | sed -e "s/\(.\{7\}\).*/\1/" | tr -d "\n" | pbcopy'
 alias empty="git commit --allow-empty -m"
 
 alias current-branch='git rev-parse --abbrev-ref HEAD'
@@ -53,27 +51,6 @@ function gg() {
 	fi
 }
 
-function mine() {
-	if [[ -n `git remote | grep mine` ]]; then
-		echo "remote mine is already set up"
-		return
-	fi
-
-	local repo_name=$(git rev-parse --show-toplevel | sed -e "s/^.*\///g")
-	local repo_path="github.com:k0kubun/${repo_name}"
-	git remote add mine $repo_path
-	git fetch mine
-	git branch --set-upstream-to=mine/`current-branch` `current-branch`
-	echo "added remote mine: ${repo_name}"
-}
-
-# Apply proxy for titech pubnet
-alias titech="git config --global http.proxy 131.112.125.238:3128"
-alias untitech="git config --global --unset http.proxy"
-
-# git-hook
-# export PATH="$HOME/.git-hook/bin:$PATH"
-
 function github-url() {
 	ruby <<-EOS
 		require 'uri'
@@ -112,22 +89,4 @@ function preq() {
 	else
 		xdg-open $url
 	fi
-}
-
-function private() {
-	git config --local user.email takashikkbn@gmail.com
-	git config --local user.name "Takashi Kokubun"
-}
-
-# ghe get
-function ghe() {
-	case $1 in
-		get )
-			# You must export $GHE_HOST in ~/.zshrc.local
-			ghq get $GHE_HOST:$2
-			;;
-		* )
-			ghq $@
-			;;
-	esac
 }
