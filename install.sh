49c2a3bfb083d496f1a9fee97d6967a2cef9421c
Takashi Kokubun <takashikkbn@gmail.com>
2021-03-02 00:45:28 -0800

    Check dein.vim instead of neobundle.vim

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [[ ! -f config/.vim/bundle/neobundle.vim/.git ]]; then
+if [ ! -f config/.config/nvim/dein/repos/github.com/Shougo/dein.vim/.git ]; then
   git submodule init && git submodule update --depth 1
 fi
 
