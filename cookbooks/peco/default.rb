da3038f58fa33bface306c31aaf7fde52b95479d
Takashi Kokubun <takashikkbn@gmail.com>
2022-07-19 21:14:09 -0700

    Support Monterey

--- cookbooks/peco/default.rb
+++ cookbooks/peco/default.rb
@@ -1,6 +1,6 @@
 github_binary 'peco' do
   repository 'peco/peco'
-  version 'v0.5.1'
+  version 'v0.5.10'
   ext = (node[:platform] == 'darwin' ? 'zip' : 'tar.gz')
   archive "peco_#{node[:os]}_amd64.#{ext}"
   binary_path "peco_#{node[:os]}_amd64/peco"
