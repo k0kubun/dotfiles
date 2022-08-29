c72de376fe907f5303bd17acc1456e1c831743fe
Takashi Kokubun <takashikkbn@gmail.com>
2022-08-28 23:28:30 -0700

    submodule is no longer relevant

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-if [ ! -f config/.config/nvim/dein/repos/github.com/Shougo/dein.vim/.git ]; then
-  git submodule init && git submodule update --depth 1
-fi
-
 bin/setup
 
 # Homebrew does not allow sudo.
