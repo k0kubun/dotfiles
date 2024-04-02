f24c82dff58843178a5024f3a71a8aab4f1946d1
Takashi Kokubun <takashikkbn@gmail.com>
2024-04-02 15:19:55 -0700

    Disable padding for WezTerm

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -7,5 +7,11 @@ config.font_size = 18.0
 
 config.enable_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 return config
