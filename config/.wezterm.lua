f890861abd42fd802114736c21afd5dc3096396d
Takashi Kokubun <takashikkbn@gmail.com>
2024-04-10 22:05:38 -0700

    Fix the font for Linux

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -2,8 +2,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.window_background_opacity = 0.85
-config.font = wezterm.font('Monaco', { weight = 'Regular' })
 config.font_size = 18.0
+if wezterm.target_triple:find('darwin') ~= nil then
+  config.font = wezterm.font('Monaco', { weight = 'Regular' })
+else
+  config.font = wezterm.font('Inconsolata')
+end
 
 config.enable_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
