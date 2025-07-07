5d36246e29dfe5a960bbf099f69988cd3a1113ab
Takashi Kokubun <takashikkbn@gmail.com>
2025-07-07 15:52:09 -0700

    Create autostart directory

--- recipes/ubuntu/xremap.rb
+++ recipes/ubuntu/xremap.rb
@@ -5,6 +5,12 @@ remote_file "#{ENV['HOME']}/.config/systemd/user/xremap.service" do
   group node[:user]
 end
 
+directory "#{ENV['HOME']}/.config/autostart" do
+  owner node[:user]
+  group node[:user]
+  mode '755'
+end
+
 remote_file "#{ENV['HOME']}/.config/autostart/xremap.desktop" do
   owner node[:user]
   group node[:user]
