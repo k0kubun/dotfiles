15bd144cbb65398868ffc439f718de8fe31a2281
Takashi Kokubun <takashikkbn@gmail.com>
2025-08-14 20:48:54 -0700

    Set up neovim as well

--- recipes/wsl/default.rb
+++ recipes/wsl/default.rb
@@ -1,3 +1,5 @@
+dotfile '.config/nvim/coc-settings.json'
+dotfile '.config/nvim/init.vim'
 dotfile '.tmux.conf'
 dotfile '.tmux.conf.local' => '.tmux.conf.linux'
 dotfile '.zsh'
@@ -6,6 +8,9 @@ dotfile '.zshrc.Linux'
 
 package 'tmux'
 package 'zsh'
+package 'neovim'
+package 'nodejs'
+package 'npm'
 
 execute "chsh -s /bin/zsh #{node[:user]}" do
   only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
