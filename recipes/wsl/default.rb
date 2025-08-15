50f95fb179862abb83d782cdb56d514195c3bc85
Takashi Kokubun <takashikkbn@gmail.com>
2025-08-14 20:42:32 -0700

    Set up zsh and tmux

--- recipes/wsl/default.rb
+++ recipes/wsl/default.rb
@@ -1,1 +1,13 @@
-puts "welcome to WSL"
+dotfile '.tmux.conf'
+dotfile '.tmux.conf.local' => '.tmux.conf.linux'
+dotfile '.zsh'
+dotfile '.zshrc'
+dotfile '.zshrc.Linux'
+
+package 'tmux'
+package 'zsh'
+
+execute "chsh -s /bin/zsh #{node[:user]}" do
+  only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
+  user 'root'
+end
