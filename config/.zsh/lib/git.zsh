8cca78af9d029dde4bfb8d2798eaaa7e0a408aa3
Takashi Kokubun <takashikkbn@gmail.com>
2025-09-09 00:15:30 -0700

    Show commit date on gl

--- config/.zsh/lib/git.zsh
+++ config/.zsh/lib/git.zsh
@@ -11,9 +11,9 @@ alias current-branch='git rev-parse --abbrev-ref HEAD'
 
 function gl(){
 	if [ $# -ne 0 ]; then
-		git --no-pager log --date=iso --no-show-signature --pretty=format:'%h %Cgreen%ad %Cblue%an %Creset%s %C(blue)%d%Creset' "$@"
+		git --no-pager log --date=iso --no-show-signature --pretty=format:'%h %Cgreen%cd %Cblue%an %Creset%s %C(blue)%d%Creset' "$@"
 	else
-		git --no-pager log --date=iso --no-show-signature --pretty=format:'%h %Cgreen%ad %Cblue%an %Creset%s %C(blue)%d%Creset' -10
+		git --no-pager log --date=iso --no-show-signature --pretty=format:'%h %Cgreen%cd %Cblue%an %Creset%s %C(blue)%d%Creset' -10
 	fi
 }
 
